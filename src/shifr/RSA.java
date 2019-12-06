fae86226862a7920530a2069e9a746782c20c0f4
Akim Molitvin <molitvin17@mail.ru>
2019-12-06 21:23:31 +0300

    Класс RSA. Добавлен метод, реализующий шифрование. #1

--- src/shifr/RSA.java
+++ src/shifr/RSA.java
@@ -23,4 +23,73 @@ public class RSA {
 
     public RSA(){}
 
+    /**
+     * Метод, который проверяет является ли число простым
+     * @param n число для проверки
+     * @return boolean возвращает результат проверки
+     */
+    private boolean isTheNumberSimple(long n) {
+        if (n < 2)
+            return false;
+
+        if (n == 2)
+            return true;
+
+        for (long i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    /**
+     * Метод, реализующий шифрования данных по алгоритму RSA
+     * @param s входные данные
+     * @param e ключ е
+     * @param n ключ n
+     * @return
+     */
+    private List<String> RSAEncode(String s, long e, long n) {
+        List<String> result = null;
+        BigInteger bi;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            int index = findIndex(characters, s.charAt(i));
+
+            bi = new BigInteger(Integer.toString(index));
+
+            bi=bi.pow((int)e);
+
+
+            BigInteger N = new BigInteger(Long.toString(n));
+
+            bi = bi.mod(N);
+
+            result.add(bi.toString());
+        }
+        return result;
+    }
+
+    /**
+     * Вспомогательная функция для поиска индекса элемента массива
+     * @param arr массив
+     * @param t элемент массива
+     * @return int возвращает индекс элемента массива
+     */
+    private int findIndex(char arr[], char t) {
+
+        // если массив пуст возвращаем -1
+        if (arr == null)
+            return -1;
+
+        // выполняем поиск элемента и возвращаем его индекс, если находим его в массиве
+        for ( int i = 0; i < arr.length; i++)
+            if (arr[i] == t)
+                return i;
+
+        //если не нашли, то возвращаем -1
+        return -1;
+    }
+    
 }
