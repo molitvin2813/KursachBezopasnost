c0938f37120e55261d02767e30be452c7ecfffca
Akim Molitvin <molitvin17@mail.ru>
2019-12-07 16:12:48 +0300

    Добавлена форма для получения сообщения

---
+++ src/GetMessage.java
@@ -0,0 +1,11 @@
+import javafx.scene.input.MouseEvent;
+
+public class GetMessage {
+    public void minimize_stage(MouseEvent mouseEvent) {
+
+    }
+
+    public void close_app(MouseEvent mouseEvent) {
+
+    }
+}
